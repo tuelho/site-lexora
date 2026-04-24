54ec783efd0ff6e277637398240914998213edba
Tuelho <tuelhoo@gmail.com>
2026-04-23 21:03:22 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
+const isCloudflarePages = process.env.CF_PAGES === '1'
 const nextConfig = {
+  ..(isCloudflarePages ? { output: 'export' } : {}),
   typescript: {
     ignoreBuildErrors: true,
   },
